0466f71b21f5c769284af960dba1137c1b461762
Rohit0445 <meenarohit458@gmail.com>
2025-04-22 11:10:11 +0530

    first commit

---
+++ rectangle.py
@@ -0,0 +1,3 @@
+length=int(input("Enter a length of rectangle"))
+breadth=int(input("Enter a breadth of rectangle"))
+print(length * breadth)
---
+++ square.py
@@ -0,0 +1,2 @@
+side=int(input("enter a number"))
+print(side*side)
