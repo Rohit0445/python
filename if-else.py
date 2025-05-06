fe6692300b4e9f2ea6928aba144887e0f4f3cc96
Rohit0445 <meenarohit458@gmail.com>
2025-05-06 11:37:23 +0530

    slicing

---
+++ if-else.py
@@ -0,0 +1,11 @@
+#a= int(input("Enter a number : "))
+#b= int(input("Enter a number : "))
+
+#if (a>b):
+ #   print("a is greater then b" )
+
+#else:
+#   print("b is greater then a ")
+
+
+
---
+++ slicing.py
@@ -0,0 +1,7 @@
+r = "Rohit Meena"
+
+print(r[0::])
+print(r[0:5:])
+print(r[0:8:2])
+print(r[::-1])
+print(r[-1:-8 :-1])
