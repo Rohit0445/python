880a4c174d83cce76545d6b80125d6b0a9cc4f26
Rohit0445 <meenarohit458@gmail.com>
2025-05-06 12:35:35 +0530

    ifElse

--- if-else.py
+++ if-else.py
@@ -1,11 +1,68 @@
+#-----------------------greater between 2 numbers-----------------
+
+
 #a= int(input("Enter a number : "))
 #b= int(input("Enter a number : "))
 
 #if (a>b):
- #   print("a is greater then b" )
+#   print("a is greater then b" )
 
 #else:
 #   print("b is greater then a ")
 
+#------------------------------------------------------------------------
 
 
+#------------------vowel or consonent ----------------------------
+
+#alpha= input("Enter a Alphabet : ")
+#vowel = ["a","e","i","o","u"]
+
+#if (alpha in vowel):
+#    print("vowel")
+
+#else:
+#    print("constant")
+ 
+#--------------------------------------------------------------------
+
+#---------------------vote -------------------------------------------
+
+
+#age = int(input("Enter your age : "))
+
+#if(age>=18):
+#    print("You are elligible for vote ")
+
+#else:
+#    print("You are not elligible for vote ")
+
+
+#----------------------------------------------------------
+
+
+#----------------------multiple of 5 or not ---------------
+
+#num = int(input("Enter your Number : "))
+
+#if(num%5==0):
+#    print("multiple of 5 ")
+
+#else:
+#    print(" not multiple of 5")
+
+#---------------------------------------------------------
+
+#----------------correct pass or not -------------------
+
+#pasword= 1234
+
+#pasword1= int(input("Enter your Password : "))
+
+#if (pasword==pasword1):
+#    print("correct pasword")
+
+#else:
+#    print("Incorrect pasword")    
+
+#-----------------------------------------------------------  
