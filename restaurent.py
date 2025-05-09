7a608bff77573eb35a822305f72412e48de11f07
Rohit0445 <meenarohit458@gmail.com>
2025-05-09 12:35:15 +0530

    path

--- restaurent.py
+++ restaurent.py
@@ -8,28 +8,44 @@ menu ={1:"burger",
 
 print(menu)
 
-order = int(input("Enter which number's dish you want to order : "))
+def customerorder():
+   order = int(input("Enter which number's dish you want to order : "))
 
-if(order>0 and order<=5):
+   if(order>0 and order<=5):
  
-    if(order==1):
-       print("you order is and your price will be RS.100")
+      if(order==1):
+          print("you order is and your price will be RS.100")
 
-    elif(order==2):
-       print("you order is pizza and your price will be RS.100")
+      elif(order==2):
+           print("you order is pizza and your price will be RS.100")
 
-    elif(order==3):
+      elif(order==3):
        print("you order is pasta and your price will be RS.100")   
     
-    elif(order==4):
-       print("you order is sanwich and your price will be RS.100")
+      elif(order==4):
+           print("you order is sanwich and your price will be RS.100")
     
-    elif(order==5):
+      elif(order==5):
        print("you order is biryani and your price will be RS.100")
     
     
-    else:
+      else:
         print("this dish is not in our menu")
 
-else:
-   print("please Enter Valid number  ")    
+   else:
+      print("please Enter Valid number  ") 
+
+  
+
+customerorder()  
+
+revisedorder=input("you want to add something in your order enter yes") 
+
+
+
+while(revisedorder=="yes"):
+   customerorder()
+   revisedorder=input("you want to add something in your order enter yes") 
+
+
+
---
+++ suitablepath.py
@@ -0,0 +1,49 @@
+num = int(input("Enter a nmber : "))
+
+if(num==1):
+    next = int(input("enter your next destination from 2,3 and 4")) 
+
+    if(next==2):
+        next = int(input("enter your next destination from 3 and 4")) 
+ 
+        if(next==3):
+            next = int(input("enter your next destination is 4")) 
+
+            if(next==4):
+                print("reached")
+
+            else:
+                print("Invalid") 
+
+        elif(next==4):
+            print("reached") 
+
+
+    elif(next==3):
+        next = int(input("enter your next destination form 2 and 4")) 
+
+        if(next==2):
+            next = int(input("enter your next destination is  4")) 
+
+            if(next==4):
+                print("reached")
+
+            else:
+                print("Invalid") 
+
+        
+        elif(next==4):
+            print("reached")
+
+        else:
+            print("Invalid")    
+
+    elif(next==4):
+        print("reached")
+
+    else:
+        print("Invalid input")    
+
+
+else:
+    print("Invalid")        
