88bd0c94ad5ae1cc27a0c4c7719931a33d886988
Rohit0445 <meenarohit458@gmail.com>
2025-05-14 12:30:49 +0530

    list

--- loops.py
+++ loops.py
@@ -15,28 +15,79 @@
 #---------------------------------------------maximum num in list -----------------------------------------------------------------------------
 
 
-li = [1,3,8,4,6]
+# li = [1,3,8,4,6]
 
-maximum = li[0]
+# maximum = li[0]
 
-for i in range(1, len(li)):
-    if maximum < li[i]:
-        maximum = li[i]
+# for i in range(1, len(li)):
+#     if maximum < li[i]:
+#         maximum = li[i]
 
-print(maximum)
+# print(maximum)
 
 
 #--------------------------------------------------------minimum num in list -----------------------------------------------------------------
 
 
-minimum = li[0]
-
-for i in range(1, len(li)):
-    if minimum > li[i]:
-        minimum = li[i]
-
-print(minimum)
 
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------
+#greatest digit 
 
+# num = 2413
+# digit = 0
+# while(num > 0):
+#     lastdigit = num % 10
+#     num = num//10
+#     if(lastdigit > digit):
+#         digit = lastdigit
+    
+
+
+# print(digit)
+
+#--------------------------------------------------------------------------------------------------------------------------------
+#smallest digit 
+
+# num = 2146
+# digit = 9
+
+# while(num > 0):
+#      digit1 = num%10
+#      if(digit1 < digit):
+#         digit = digit1
+#      num = num//10
+        
+
+# print(digit)
+    
+#------------------------------------------------------------------------------------------------------------------------
+#list in assending order.
+
+
+# li = [1,8,6,9,4,3,7,2,5 ]
+
+# for i in range(len(li)):
+    
+#     for j in range(i+1, len(li)):
+#         if(li[j] < li[i]):
+#             temp = li[i]
+#             li[i] = li[j]
+#             li[j] = temp    
+
+# print(li)
+
+#----------------------------------------------------------------------------------------------------------------------------------
+#dessanding order 
+
+li = [1,8,6,9,4,3,7,2,5 ]
+
+for i in range(len(li)):
+    
+    for j in range(i+1, len(li)):
+        if(li[j] > li[i]):
+            temp = li[i]
+            li[i] = li[j]
+            li[j] = temp    
+
+print(li)
