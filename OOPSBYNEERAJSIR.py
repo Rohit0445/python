251022ba671c2a030076b7e3765e6a63ca39a2a9
Rohit0445 <meenarohit458@gmail.com>
2025-07-12 11:29:41 +0530

    initial commit

--- OOPSBYNEERAJSIR.py
+++ OOPSBYNEERAJSIR.py
@@ -78,28 +78,92 @@
 
 # ============================instence Veriable ==================
 
-class Student :
-    def __init__(self,name):
-        self.n = name   #decleration of instance variable
+# class Student :
+#     def __init__(self,name):
+#         self.n = name   #decleration of instance variable
     
-    def addnow(self,age):
-        self.a = age   #decleration
+#     def addnow(self,age):
+#         self.a = age   #decleration
 
-    def show(self):
-        print(self.n,self.a,self.city)  #calling
+#     def show(self):
+#         print(self.n,self.a,self.city)  #calling
+
+# obj = Student('Rohit')
+
+# obj.addnow(24)
+
+# obj.city='bhopal'  #decleration
+
+# obj.show()  
+
+
+# print(obj.n,obj.a,obj.city)    #calling 
+
+
+# ============================Static Veriable ==================
+
+
+# class Student :
+#     school = 'SHSS'
+
+#     def __init__(self,name):
+#         self.n = name   #decleration of instance variable
+#         Student.gread = "10th"
+
+
+    
+#     def addnew(self):
+#         Student.principal= 'python'   #decleration
+
+#     def show(self):
+#         print(Student.school,Student.gread,Student.principal)  
+
+# Student.city = 'bhopal'
+
+# obj = Student('Rohit')
+
+# obj.addnew() 
+# obj.show() 
+# print(f'name = {obj.n}, School = {obj.school} , city = {obj.city}, class = {obj.gread} , principal = {obj.principal}')
+
+
+
+#============================local Variable==================
+
+# class Student :
+#     school = 'SSRHSS'
+#     def __init__(self):
+#         x = 10
+
+#     def new(self):
+#         print(x) # x is not defined beacause x is a loccal variable 
+
+# obj = Student()
+# obj.new()
+
+
+#---------------------------------------   METHODS   ----------------------------------------------
+
+class Student:
+    school = "SAKET SHISHU RRANJAN HIGHER SECONDARY SCHOOL VIDISHA"
+    greade = "12th"
+
+    def __init__(self,name):
+        self.n=name
+
+    @classmethod 
+    def update(cls,x,y):   #update class Variable or create new variable
+        cls.greade=x
+        cls.principle=y
 
 obj = Student('Rohit')
+print(obj.greade)
 
-obj.addnow(24)
+obj.update('10th','RAGINI PANDEY') #update class Variable or create new variable
+print(obj.greade)
+print(obj.principle)
 
-obj.city='bhopal'  #decleration
+print(obj.school)
+print(obj.n)
 
-obj.show()  
-
-
-print(obj.n,obj.a,obj.city)    #calling 
-
-
-
-
-
+ 
