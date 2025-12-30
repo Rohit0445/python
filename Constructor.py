faa2c24a2a18901a71f62d0fe09fd0c9b3271dec
Rohit0445 <meenarohit458@gmail.com>
2025-12-30 14:52:15 +0530

    initiL

--- Constructor.py
+++ Constructor.py
@@ -1,8 +1,22 @@
-class new:
-    def __init__(self,x):
-        print(id(x))
-        print("Constructor Called")
+# class new:
+#     def __init__(self,x):
+#         print(id(x))
+#         print("Constructor Called")
 
-obj = new(10)
+# obj = new(10)
 
 # n number of constructor bana sakte hai pr last bala call hota hai automatiacally
+# kyunki same name k function overload hota hai jo python support nAHI karta 
+
+# ____________________instance variable-----------------
+
+class info:
+    def __init__(self,name,age):
+        self.name = name #INSTANCE VARIABLE -------------------- left side ka veriable hota hai right side value 
+        self.age = age
+        print(name)
+        print(age)
+
+
+obj = info("Rohit",24)
+obj1 = info("Ajay",24)
